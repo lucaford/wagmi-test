c2d1dab3b1da871afdf5a33928027f17169219ae
Luca Ford <lford@BA-MAC-123.local>
2022-11-24 22:06:55 -0300

    Setup and wagmi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { configureChains, chain } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { provider, webSocketProvider } = configureChains(
+    [chain.mainnet, chain.polygon],
+    [publicProvider()]
+  );
+  return <Component {...pageProps} />;
 }
